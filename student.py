8f58e27a72e7b6af42fcf1affa93d258ee51c8c5
EvanMacD <100422820+EvanMacD@users.noreply.github.com>
2022-03-29 10:25:28 -0400

    Update student.py

--- student.py
+++ student.py
@@ -9,5 +9,8 @@ def main():
 
     elif number %3==0:
         print("fizz")
+        
+    else:
+        print("You have entered a number that isn't divisble by 3 or 5")
 if __name__ =='__main__':
     main()
