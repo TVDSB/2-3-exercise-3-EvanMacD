54ac6ab0f6a82594146b6350b2855a2b4988458c
EvanMacD <100422820+EvanMacD@users.noreply.github.com>
2022-03-29 10:28:23 -0400

    Update student.py

--- student.py
+++ student.py
@@ -2,15 +2,15 @@ def main():
     number = int(input("Enter a number here \n"))
 
     if number %15==0:
-        print("fizzbuzz")
+      print("fizzbuzz")
 
     elif number%5==0:
-        print("buzz")
+      print("buzz")
 
     elif number %3==0:
-        print("fizz")
+      print("fizz")
         
     else:
-        print("You have entered a number that isn't divisble by 3 or 5")
+      print("You have entered a number that isn't divisble by 3 or 5")
 if __name__ =='__main__':
     main()
